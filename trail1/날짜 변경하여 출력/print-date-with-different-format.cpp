23097fc405fca2dcc8e76587b08a9db36e633427
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-08-13 11:27:57 +0900

    [Passed] 날짜 변경하여 출력 (2ms, 0MB)

---
+++ trail1/날짜 변경하여 출력/print-date-with-different-format.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a,b,c;
+    char d = '-';
+
+    cin >> a;
+    cin.get();
+    cin >> b;
+    cin.get();
+    cin >> c;
+
+    cout << b << d << c << d << a;
+    return 0;
+}
