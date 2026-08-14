c810396d03a39dd6ecbd5ac4f1611c935a661591
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-08-14 09:54:03 +0900

    [Passed] 삼항연산자 (2ms, 0MB)

---
+++ trail1/삼항연산자/ternary-operator.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int score;
+    string grade;
+    cin >> score;
+
+    grade = score == 100 ? "pass" : "failure";
+
+    cout << grade;
+    
+    return 0;
+}
