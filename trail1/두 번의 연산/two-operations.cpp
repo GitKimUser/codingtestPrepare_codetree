6b9a6acb83e43138f5bb2de9987d9ce042366867
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-08-19 10:23:16 +0900

    [Passed] 두 번의 연산 (2ms, 0MB)

---
+++ trail1/두 번의 연산/two-operations.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A;
+    cin >> A;
+
+    if(A % 2 != 0){
+        A += 3;
+    }
+
+    if(A % 3 == 0){
+        A /= 3;
+    }
+
+    cout << A;
+    return 0;
+}
