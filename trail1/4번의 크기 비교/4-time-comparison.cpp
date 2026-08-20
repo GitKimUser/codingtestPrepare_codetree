21e9959a9e6f3400c44f5f11e02655c7e4a17955
newBeeKim <67673263+newBeeKim@users.noreply.github.com>
2026-08-20 10:23:44 +0900

    [Passed] 4번의 크기 비교 (2ms, 0MB)

---
+++ trail1/4번의 크기 비교/4-time-comparison.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int A,B,C,D,E;
+    cin >> A >> B >> C >> D >> E;
+
+    cout << (A > B) << endl;
+    cout << (A > C) << endl;
+    cout << (A > D) << endl;
+    cout << (A > E) << endl;
+
+    return 0;
+}
